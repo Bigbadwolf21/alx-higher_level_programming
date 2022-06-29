24aeb658956859570726f3d82b883f6d4cde0272
Ayodele Ayomide <ayodeledprincipal@gmail.com>
2022-06-29 18:20:27 +0100

    Alphabt Soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Print the ASCII Alphabet in lowercase with letter e and q
+"""
 for letter in range(97, 123):
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
+    if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
