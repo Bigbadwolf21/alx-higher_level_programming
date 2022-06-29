4cf0f7f4fe317009bf794ca3a3a2738c6f339062
Ayodele Ayomide <ayodeledprincipal@gmail.com>
2022-06-29 20:35:40 +0100

    Power of

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+
 def pow(a, b):
-    """
-    Calculate a raised to the power of b, and return the value
-    """
-    return (a**b) 
+    """Return a to the power of b."""
+    return (a ** b)
