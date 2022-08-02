a5c3f817f19c0f563b0d69bc48424a2feeb59547
Ayodele Ayomide <ayodeledprincipal@gmail.com>
2022-08-02 03:06:10 +0100

    My List

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-# 1-my_list.py
-"""Defines an inherited list class MyList.
-"""
-
+"""Define MyList class"""
 
 class MyList(list):
-    """Implements sorted printing for the built-in list class.
-    """
-
-    def print_sorted(self):
-        """Print a list in sorted ascending order.
-    """
-        print(sorted(self))
+	"""Representation of MyList class"""
+	
+	def print_sorted(self):
+		"""print list in sorted order"""
+		print(sorted(self))
